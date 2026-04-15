d0c3780882ee5a694992a79f98d3be5209b88988
luiz sette <luiz.fernando.sette@gmail.com>
2026-04-15 07:53:04 -0300

    Course

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -13,3 +13,4 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
+INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Java Basics', 'https://example.com/java-basics.jpg', 'https://example.com/java-basics-gray.jpg');
